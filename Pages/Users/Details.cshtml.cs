3ca01d174ec964b9f1093ecdab2ee1b268053727
Gisnashaji <jisnashaji2008@gmail.com>
2024-04-04 23:13:51 +0530

    second commit

---
+++ Pages/Users/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Employee.Data;
+using Employee.Model;
+
+namespace Employee.Pages.Users
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Employee.Data.EmployeeContext _context;
+
+        public DetailsModel(Employee.Data.EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        public User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+    }
+}
